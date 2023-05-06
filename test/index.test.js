8a37dedbc820c7486173f9ec46e8a2273658fe7f
Chris Oakman <chris@oakmac.com>
2023-05-06 01:35:59 -0500

    load test cases using .eno files

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,48 @@
-const foo1 = require('../lib')
+const fs = require('fs-plus')
+const path = require('path')
 
-test('test1', () => {
-  expect(foo1('aaa', 'bbb')).toBe('zzz')
+const enolib = require('enolib')
+
+const rootDir = path.join(__dirname, '../')
+
+const isEnoFile = (f) => {
+  return path.extname(f) === '.eno'
+}
+
+// returns an Array of all the .eno files in the test_parser/ folder
+const enoFilesInTestParserDir = () => {
+  const allFiles = fs.readdirSync(path.join(rootDir, 'test_parser/'))
+  return allFiles.filter(isEnoFile)
+}
+
+let allTestCases = []
+
+// parses a test .eno file and returns an Array of Objects like { input, expected, name }
+const parseTestFile = (txt) => {
+  const document = enolib.parse(txt)
+  const sections = document.sections()
+
+  const tests = []
+  sections.forEach((s) => {
+    tests.push({
+      name: s._instruction.key,
+      input: s.field('Input').requiredStringValue(),
+      expected: s.field('Expected').requiredStringValue()
+    })
+  })
+  return tests
+}
+
+enoFilesInTestParserDir().forEach((f) => {
+  const testFileContents = fs.readFileSync(path.join(rootDir, 'test_parser', f), 'utf8')
+  const testsInFile = parseTestFile(testFileContents)
+  allTestCases = allTestCases.concat(testsInFile)
 })
+
+// const foo1 = require('../lib')
+
+// test('test1', () => {
+//   expect(foo1('aaa', 'bbb')).toBe('zzz')
+// })
+
+console.log(allTestCases)
