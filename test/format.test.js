4849f2907968177d7f56b550b59688be81a03217
Chris Oakman <chris@oakmac.com>
2024-10-12 14:39:39 -0500

    WIP WIP WIP

--- test/format.test.js
+++ test/format.test.js
@@ -68,7 +68,7 @@ test('All test_format/ cases should have unique names', () => {
 
 // dev convenience: set this to true and add specific test cases
 // only those cases will run
-const onlyRunSpecificTests = true
+const onlyRunSpecificTests = false
 const specificTests = new Set()
 specificTests.add('GitHub Issue #110 - tagged literal indentation')
 
