c0851bc7e0ba81b48615f4923f34f634bf568b47
Chris Oakman <chris@oakmac.com>
2024-10-13 11:01:13 -0500

    WIP: another test passing

--- test/parse_ns.test.js
+++ test/parse_ns.test.js
@@ -58,7 +58,7 @@ test('All test_parse_ns/ cases should have unique names', () => {
 
 // dev convenience: set this to true and add specific test cases
 // only those cases will run
-const onlyRunSpecificTests = true
+const onlyRunSpecificTests = false
 const specificTests = new Set()
 // specificTests.add('your test case here')
 
