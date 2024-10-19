d3247cca27ad46d96b1435a86a7480dacb441c5e
Chris Oakman <chris@oakmac.com>
2024-10-19 13:00:29 -0500

    GitHub Issue #103 - convert CRLF to LF

---
+++ test_format/line_endings/output.clj
@@ -0,0 +1,24 @@
+(ns wolframite.base.cep
+  (:require
+   [wolframite.base.convert :as convert]
+   [wolframite.base.evaluate :as evaluate]
+   [wolframite.base.parse :as parse]
+   [wolframite.lib.options :as options]))
+
+(defn- identity-first [x & _] x)
+
+(defn cep
+  "Convert-Evaluate-Parse pipeline.
+  Convert:  from clj data to jlink Expr
+  Evaluate: the Expr on (some) Wolfram Engine
+  Parse:    returned result into clj data.
+  Each stage can be skipped with appropriate `opts` `:flag` e.g. `:no-parse`"
+  [expr {:keys [flags]
+         :as   opts}]
+  (let [convert  (if (options/flag?' flags :convert)   convert/convert   identity-first)
+        evaluate (if (options/flag?' flags :evaluate)  evaluate/evaluate identity-first)
+        parse    (if (options/flag?' flags :parse)     parse/parse       identity-first)]
+    (-> expr
+        (convert  opts)
+        (evaluate opts)
+        (parse    opts))))
