2c2741ca622b9b25a6cec6ad32553e5e814eedd2
Chris Oakman <chris@oakmac.com>
2024-10-20 00:06:05 -0500

    fixup

--- test/parse_ns.test.js
+++ test/parse_ns.test.js
@@ -58,11 +58,9 @@ test('All test_parse_ns/ cases should have unique names', () => {
 
 // dev convenience: set this to true and add specific test cases
 // only those cases will run
-const onlyRunSpecificTests = true
+const onlyRunSpecificTests = false
 const specificTests = new Set()
 // specificTests.add('your test case here')
-specificTests.add('portal.runtime.cson before')
-specificTests.add('portal.runtime.cson after')
 
 const ignoreSomeTests = false
 const ignoreTests = new Set()
