4a981232331cd2c3cfca476badb128fa677b4215
Chris Oakman <chris@oakmac.com>
2024-11-21 23:45:25 -0600

    WIP - add failing test case

--- test/format.test.js
+++ test/format.test.js
@@ -64,9 +64,10 @@ test('All test_format/ cases should have unique names', () => {
 
 // dev convenience: set this to true and add specific test cases
 // only those cases will run
-const onlyRunSpecificTests = false
+const onlyRunSpecificTests = true
 const specificTests = new Set()
 // specificTests.add('your test case here')
+specificTests.add('GitHub Issue #162 - bug with commas')
 
 const ignoreSomeTests = true
 const ignoreTests = new Set()
