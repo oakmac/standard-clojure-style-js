b0845411618257b127c8c0d4c29da19914b56442
Chris Oakman <chris@oakmac.com>
2024-11-28 11:15:21 -0600

    WIP WIP WIP

--- test/format.test.js
+++ test/format.test.js
@@ -67,7 +67,7 @@ test('All test_format/ cases should have unique names', () => {
 const onlyRunSpecificTests = false
 const specificTests = new Set()
 // specificTests.add('your test case here')
-specificTests.add('GitHub Issue #162 - bug with commas 1')
+specificTests.add('respect commas to hold parens 4')
 
 const ignoreSomeTests = true
 const ignoreTests = new Set()
