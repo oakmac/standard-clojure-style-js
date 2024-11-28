a8989750c26bf65504bc0507ed02aaa747b58c90
Chris Oakman <chris@oakmac.com>
2024-11-28 11:36:19 -0600

    WIP - ugly, but it works

--- test/format.test.js
+++ test/format.test.js
@@ -64,7 +64,7 @@ test('All test_format/ cases should have unique names', () => {
 
 // dev convenience: set this to true and add specific test cases
 // only those cases will run
-const onlyRunSpecificTests = true
+const onlyRunSpecificTests = false
 const specificTests = new Set()
 // specificTests.add('your test case here')
 // specificTests.add('respect commas to hold parens 4')
